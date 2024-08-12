93d961a5bb44accf94f985861133cbf4882acf0d
roby870 <roby870@hotmail.com>
2024-08-11 23:21:43 -0300

    delete unused import

--- frontend/src/NavigationBar/index.js
+++ frontend/src/NavigationBar/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
