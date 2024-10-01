720ec0b730b272910f559e33a3df8ca57825d6cd
roby870 <roby870@hotmail.com>
2024-09-30 21:56:34 -0300

    update backend requirements

--- backend/angelcam/account/views.py
+++ backend/angelcam/account/views.py
@@ -5,7 +5,7 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from .models import CustomUser
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 class ExternalTokenAuthentication(BaseAuthentication):
